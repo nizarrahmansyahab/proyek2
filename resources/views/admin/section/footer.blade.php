7dd18b32300b79c51bd484bda01ebf0a791abedc
nizarsyahab <dalahcapek@gmail.com>
2020-10-29 01:54:30 +0700

    pembuatan e-commerce

---
+++ resources/views/admin/section/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="copyright bg-white">
+              <p>
+                &copy; <span id="copy-year">2020</span> Copyright by
+                <a
+                  class="text-primary"
+                  href="http://www.iamabdus.com/"
+                  target="_blank"
+                  >shopei</a
+                >.
+              </p>
+            </div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/layout', function () {
+    return view('admin.layout');
+});
+Route::get('/specialsoffer', function () {
+    return view('specialsoffer');
+});
+Route::get('/delivery', function () {
+    return view('delivery');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/admin', function () {
+    return view('admin.admin');
+});
+Route::get('/specialsoffer2', function () {
+    return view('specialsoffer2');
+});
+Route::get('/delivery2', function () {
+    return view('delivery2');
+});
+Route::get('/contact2', function () {
+    return view('contact2');
+});
+Route::get('/beranda', function () {
+    return view('beranda');
+});
+Route::get('/produk', function () {
+    return view('produk');
+});
+Route::get('/kategori', function () {
+    return view('kategori');
+});
+Route::get('/promo', function () {
+    return view('promo');
+});
+Route::get('/login', function () {
+    return view('login');
+});
